e537b2becd732189e28003b1e7c8836f20082beb
Pablo Alejandro Bertoli <pbertoli@alumno.unlam.edu.ar>
2018-11-11 17:17:03 -0300

    Agregar archivos de proyecto.

---
+++ ModeloDeParcial2/Controllers/MaratonController.cs
@@ -0,0 +1,59 @@
+﻿using ModeloDeParcial2.Models;
+using ModeloDeParcial2.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ModeloDeParcial2.Controllers
+{
+    public class MaratonController : Controller
+    {
+        MaratonService ms = new MaratonService();
+        // GET: Maraton
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult CrearMaraton()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult CrearMaraton(Maraton maraton)
+        {
+            ms.CrearMaraton(maraton);
+            return RedirectToAction("ListarMaratones");
+        }
+
+        public ActionResult ListarMaratones()
+        {
+            return View(ms.ObtenerListaMaratones());
+        }
+
+        public ActionResult Editar(int id)
+        {
+            Maraton m = ms.GetMaratonById(id);
+            return View("CrearMaraton", m);
+        }
+
+        [HttpPost]
+        public ActionResult Editar(Maraton maraton)
+        {
+            //Maraton m = ms.GetMaratonById(id);
+
+            ms.EditarMaraton(maraton);
+            return RedirectToAction("ListarMaratones");
+        }
+
+        public ActionResult Eliminar(int id)
+        {
+            ms.EliminarMaraton(id);
+            return RedirectToAction("ListarMaratones");
+        }
+    }
+}
---
+++ ModeloDeParcial2/Services/MaratonService.cs
@@ -0,0 +1,48 @@
+﻿using ModeloDeParcial2.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ModeloDeParcial2.Services
+{
+    public class MaratonService
+    {
+        MaratonEntities ctx = new MaratonEntities();
+
+        public List<Maraton> ObtenerListaMaratones()
+        {
+            var query = (from m in ctx.Maraton
+                         where m.Cantidad_Participante > 10
+                         select m).ToList();
+
+            return query;
+        }
+        public void CrearMaraton(Maraton maraton)
+        {
+            ctx.Maraton.Add(maraton);
+            ctx.SaveChanges();
+        }
+
+        public Maraton GetMaratonById(int id)
+        {
+            Maraton maraton = ctx.Maraton.Find(id);
+            return maraton;
+        }
+
+        public void EditarMaraton(Maraton maraton)
+        {
+            Maraton m = GetMaratonById(maraton.id);
+            m.nombre = maraton.nombre;
+            m.Cantidad_Participante = maraton.Cantidad_Participante;
+            ctx.SaveChanges();
+        }
+
+        public void EliminarMaraton(int id)
+        {
+            Maraton maraton = ctx.Maraton.Find(id);
+            ctx.Maraton.Remove(maraton);
+            ctx.SaveChanges();
+        }
+    }
+}
